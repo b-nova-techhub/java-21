3a35b2c5bdcf163c8bea0a4f80e60a616d5d6d0e
ttrapp <tom.trapp@b-nova.com>
2024-02-14 21:43:48 +0100

    java-21

--- src/main/java/com/bnova/techhub/string_templates/StringTemplate.java
+++ src/main/java/com/bnova/techhub/string_templates/StringTemplate.java
@@ -7,22 +7,34 @@ import java.util.stream.Collectors;
 
 
 void main() {
+
+	int x = 10;
+	int y = 20;
+
+	String a = x + " times " + y + " = " + x * y;
+	String b = String.format("%d times %d = %d", x, y, x * y);
+	String c = "%d times %d = %d".formatted(x, y, x * y);
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+
+	// The new way
+	System.out.println(STR."\{x} times \{y} = \{x * y}");
+
 	var name = "Tom";
 	var city = "Basel";
-	var x = new Random().nextInt(100);
+	var random = new Random().nextInt(100);
 
 	System.out.println(STR."Hello \{name}, welcome to \{city}!");
 	System.out.println(STR."Hello \{name.toLowerCase().charAt(0)}, welcome to \{city.toUpperCase()}!");
 
-	System.out.println(STR."Your random number is \{x}.");
-	System.out.println(STR."Your random number is \{x} and the half of it is \{x/2}.");
+	System.out.println(STR."Your random number is \{random}.");
+	System.out.println(STR."Your random number is \{random} and the half of it is \{random/2}.");
 
 	System.out.println(STR."The podcast name is \{getPodcastName()}.");
 
 
-	StringTemplate.Processor<String, RuntimeException> BNOVA = template -> {
-		return template.fragments().stream().map(String::toUpperCase).collect(Collectors.joining()) + STR."; check out the podcast \{getPodcastName()}";
-	};
+	StringTemplate.Processor<String, RuntimeException> BNOVA = template -> template.fragments().stream().map(String::toUpperCase).collect(Collectors.joining()) + STR."; check out the podcast \{getPodcastName()}";
 
 	System.out.println(BNOVA."hello from basel, the beautiful city 'am rhy'");
 
